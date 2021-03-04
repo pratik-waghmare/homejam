b2490b318fa56fae2972da5184a570d9a01fb624
unknown <pratikwaghmare000@gmail.com>
2021-03-04 09:33:33 +0530

    Added flags in review card

--- src/components/ProgressLabel.js
+++ src/components/ProgressLabel.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./ProgressLabel.scss";
+import React from 'react';
+import './ProgressLabel.scss';
 
 function ProgressLabel({ labelInfo }) {
   const offset = 440 - (440 * (labelInfo.count / 10)) / 100;
@@ -8,16 +8,22 @@ function ProgressLabel({ labelInfo }) {
     <div className="ProgressLabel">
       <svg>
         <circle cx="70" cy="70" r="70"></circle>
-        <circle
-          cx="70"
-          cy="70"
-          r="70"
-          style={{ strokeDashoffset: offset }}
-        ></circle>
+        {labelInfo.type === 'likes' && (
+          <circle
+            cx="70"
+            cy="70"
+            r="70"
+            style={{ strokeDashoffset: offset }}
+          ></circle>
+        )}
       </svg>
       <div className="ProgressLabel__content">
         <img src={labelInfo.icon} alt="heart-icon" />
-        <h4>{labelInfo.count}</h4>
+        <h4>
+          {labelInfo.isPositive && '+'}
+          {labelInfo.count}
+          {labelInfo.type === 'users' && 'K'}
+        </h4>
         <p>{labelInfo.title}</p>
       </div>
     </div>
--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./ReviewCard.scss";
+import React from 'react';
+import './ReviewCard.scss';
 
 function ReviewCard({ review }) {
   return (
@@ -7,7 +7,10 @@ function ReviewCard({ review }) {
       <img src={review.imgUrl} alt="benny" />
       <div className="header">
         <h1 className="heading">{review.name}</h1>
-        <h4 className="city">{review.city}</h4>
+        <div className="city__wrapper">
+          <img src={review.flag} alt={review.city} />
+          <h4 className="city">{review.city}</h4>
+        </div>
       </div>
       <div className="content">
         <p>{review.text}</p>
